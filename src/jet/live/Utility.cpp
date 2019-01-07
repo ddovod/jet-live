41f4883a19fadfcd94cee2a105292797d6dd60cf
ddovod <ddovod@gmail.com>
2019-01-07 17:48:35 +0300

    fixing this executable path string length

--- src/jet/live/Utility.cpp
+++ src/jet/live/Utility.cpp
@@ -8,9 +8,8 @@ namespace jet
     {
         std::string pathString;
         auto pathLength = static_cast<size_t>(wai_getExecutablePath(nullptr, 0, nullptr));
-        pathString.resize(pathLength + 1);
+        pathString.resize(pathLength);
         wai_getExecutablePath(const_cast<char*>(pathString.data()), static_cast<int>(pathLength), nullptr);
-        pathString[pathLength] = '\0';
         return pathString;
     }
 }
