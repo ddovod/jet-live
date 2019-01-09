eff7eb16e7ae38738f7fbb1844d9c1569dd23d00
ddovod <ddovod@gmail.com>
2019-01-10 00:59:04 +0300

    adding test for static variables

---
+++ tests/src/StaticVariable_test.cpp
@@ -0,0 +1,21 @@
+
+#include <catch.hpp>
+#include <iostream>
+#include <thread>
+#include "StaticVariable.hpp"
+#include "Globals.hpp"
+#include "WaitForReload.hpp"
+
+TEST_CASE("Relocation of static variable", "[function]")
+{
+    auto beforeReload = getNext();
+    REQUIRE(beforeReload.first == 0);
+    REQUIRE(beforeReload.second == 10);
+
+    std::cout << "JET_TEST: disable(11:1); enable(11:2)" << std::endl;
+    waitForReload();
+
+    auto afterReload = getNext();
+    REQUIRE(afterReload.first == 1);
+    REQUIRE(afterReload.second == 11);
+}
