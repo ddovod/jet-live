e0604094738003e9ff55da8c9b49de28d2f0053d
ddovod <ddovod@gmail.com>
2019-01-23 00:31:17 +0300

    trying to break test

--- tests/src/utility/UndefinedSymbol2.cpp
+++ tests/src/utility/UndefinedSymbol2.cpp
@@ -1,9 +1,7 @@
 
 #include "UndefinedSymbol2.hpp"
 
-static int undefinedSomeStaticVariable2 = 42;
-
 int UndefinedSomeClass::undefinedGetValue2()
 {
-    return undefinedSomeStaticVariable2++;
+    return m_value++;
 }
--- tests/src/utility/UndefinedSymbol2.hpp
+++ tests/src/utility/UndefinedSymbol2.hpp
@@ -5,4 +5,7 @@ class UndefinedSomeClass
 {
 public:
     int undefinedGetValue2();
+
+private:
+    int m_value = 42;
 };
