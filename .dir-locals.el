ba28f5225486d2ed678c328b42f86e35d0ff45d5
ddovod <ddovod@gmail.com>
2019-02-23 15:20:31 +0300

    shorter .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,16 +1,9 @@
 
-((nil . ((eval .
-               (progn
-                 ;; Reload code in `example` application
-                 (defun reload-code-in-app ()
-                   (interactive)
-                   (async-shell-command "kill -SIGUSR1 $(pgrep example)")
-                   )
-
-                 ;; Do not show that annoying popup when shell command is finished
-                 (add-to-list 'display-buffer-alist
-                              (cons "\\*Async Shell Command\\*.*" (cons #'display-buffer-no-window nil)))
-
-                 (global-set-key (kbd "C-c c r") 'reload-code-in-app)
-                 )))))
+((nil . ((eval . (progn
+                   (defun reload-code-in-app ()
+                     (interactive)
+                     (progn
+                       (save-some-buffers)
+                       (async-shell-command "pkill -SIGUSR1 example")))
+                   (global-set-key (kbd "C-c c r") 'reload-code-in-app))))))
 
